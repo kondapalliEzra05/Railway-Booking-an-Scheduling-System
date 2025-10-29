ef8f056c4800c750659cffb8fdabe0bf2f8abebc
kondapalliEzra05 <ezrareddy11@gmail.com>
2025-10-30 01:37:55 +0530

    Delete Railway-reservation-Django-master/Railway-reservation-Django-master directory

--- Railway-reservation-Django-master/Railway-reservation-Django-master/README.md
+++
@@ -1,1 +0,0 @@
-# Railway-reservation-Django
--- Railway-reservation-Django-master/Railway-reservation-Django-master/reservation/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import trains,person
-
-admin.site.register(trains)
-admin.site.register(person)
